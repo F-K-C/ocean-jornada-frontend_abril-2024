bde6036734309cb712c80f54229416f93f434000
F-K-C <fakocarvalho@gmail.com>
2024-04-25 22:07:09 +0000

    Adição de Props para o componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      <Card item={item1}/>
+      <Card item={item2}/>      
     </>
   )
 }
