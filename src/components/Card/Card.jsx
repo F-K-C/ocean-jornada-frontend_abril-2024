cd82fc15b8de5bbf7f4acc8b45ccb2a5141b39c4
F-K-C <fakocarvalho@gmail.com>
2024-04-25 23:09:49 +0000

    Exibição de Tags na Array

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,16 @@ import Tag from "../Tag/Tag"
 export default function Card(props) {
     const item = props.item
 
+    console.log(item)
+
     return (
         <div className="card">
             <h2>{item.name}</h2>
 
             <div className='tags'>
+                {item.tags.map(function(tag){
+                    return <Tag text={tag} />
+                })}
                 <Tag text='Status: Vivo'/>
                 <Tag text='Espécie: Humana'/>
                 <Tag text='Origem: Terra c-137'/>
